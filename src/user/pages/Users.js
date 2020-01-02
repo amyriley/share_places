ad80a3761e685f78ac81faa20f7a78af235c337c
amrmynd <ariley.amy@gmail.com>
2020-01-02 18:55:01 +0100

    adding header, nav-links and side-drawer

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
+    const USERS = [
+        { 
+            id: "id1", 
+            name: "Bob Jones", 
+            image: "https://pixabay.com/photos/cat-sweet-kitty-animals-feline-323262/", 
+            places: 3
+        }
+    ];
+
     return ( 
-        <h2>Users works!</h2>
+        <UsersList items={USERS} />
      );
 }
  
